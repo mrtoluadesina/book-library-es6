1fe9ed777b9d618dd981f323043d3a1f8d17c246
Tolu Adesina <tadesina90@gmail.com>
2019-05-08 17:11:06 +0100

    User Constructor updated to have creation happen when an instance is created and then createUser Method is deleted

--- src/users/users.js
+++ src/users/users.js
@@ -1,16 +1,13 @@
 var db = require('../../db');
 
-function User() {
+function User(name) {
+  this.name = name;
   this.id = db.users.length > 0 ? db.users[db.users.length - 1].id + 1 : 1;
+  db.users.push(this);
 }
 
 User.prototype = {
   constructor: User,
-  createUser: function(name) {
-    this.name = name;
-    db.users.push(this);
-    return 'User Created';
-  },
   readUser: function() {
     return this;
   }
