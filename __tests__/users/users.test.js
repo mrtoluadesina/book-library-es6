71b336b3c0f70f04737b3bf48dc1b28c2b6a9ba4
Tolu Adesina <tadesina90@gmail.com>
2019-05-08 17:09:59 +0100

    Test Cases Updated - with user created at the Constructor

--- __tests__/users/users.test.js
+++ __tests__/users/users.test.js
@@ -3,30 +3,23 @@ var User = require('../../src/users/users');
 
 describe('User Constructor Tests', function() {
   it('checks the id autoincrement if it works properly', function() {
-    var jon = new User();
-    jon.createUser('snow');
+    var jon = new User('snow');
     expect(jon.id).toBe(1);
   });
 
-  it('checks for an instance of the constructor', function() {
-    var snow = new User();
+  it('checks if a user is created from the constructor', function() {
+    var snow = new User('jon');
     expect(snow instanceof User).toBeTruthy();
   });
 
-  it('checks if a user is created', function() {
-    var jon = new User();
-    expect(jon.createUser('jon')).toMatch('User Created');
-  });
-
   it('checks if a user can read his details', function() {
-    var arya = new User();
+    var arya = new User('stark');
     var expected = {'name': expect.stringMatching(/^[a-z]+$/i)};
-    arya.createUser('stark');
     expect(arya.readUser()).toEqual(expect.objectContaining({'name': 'stark'}));
     expect(arya.readUser()).toEqual(expect.objectContaining(expected));
   });
 
   it('checks if the users details gets updated', function() {
-    
-  })
+    var arya = new User();
+  });
 });
