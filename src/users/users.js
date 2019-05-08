2203611c9d10282d6f226a9ed88e91e513ffc301
Tolu Adesina <tadesina90@gmail.com>
2019-05-08 16:15:23 +0100

    createUser method added to the User Constructor Prototype

--- src/users/users.js
+++ src/users/users.js
@@ -4,6 +4,13 @@ function User() {
   this.id = db.users.length > 0 ? db.users[db.users.length - 1].id + 1 : 1;
 }
 
-
+User.prototype = {
+  constructor: User,
+  createUser: function(name) {
+    this.name = name;
+    db.users.push(this);
+    return 'User Created';
+  }
+}
 
 module.exports = User;
