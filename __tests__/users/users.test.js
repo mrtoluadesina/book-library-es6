a383b0efc297c7742489864f1e1116b423c22dd6
Tolu Adesina <tadesina90@gmail.com>
2019-05-08 16:14:13 +0100

    Test Case - A test to assert the id autoincrement functionality

--- __tests__/users/users.test.js
+++ __tests__/users/users.test.js
@@ -1,6 +1,13 @@
+var db = require('../../db');
 var User = require('../../src/users/users');
 
 describe('User Constructor Tests', function() {
+  it('checks the id autoincrement if it works properly', function() {
+    var jon = new User();
+    jon.createUser('snow');
+    expect(jon.id).toBe(1);
+  });
+
   it('checks for an instance of the constructor', function() {
     var jon = new User();
     expect(jon instanceof User).toBeTruthy();
