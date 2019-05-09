1e1d7fa92128d524715deb8c9aee14a3f1978c07
Tolu Adesina <tadesina90@gmail.com>
2019-05-09 01:26:19 +0100

    Teacher Constructor created and inherits from the User Constructor

--- src/users/teachers.js
+++ src/users/teachers.js
@@ -1,9 +1,12 @@
 var db = require('../../db');
+var inherit = require('../extend');
 var User = require('../users/users');
 
 function Teacher(name) {
   User.call(this, name);
-  
+  this.role = 'teacher';
 }
+
+inherit(Teacher, User);
 
 module.exports = Teacher;
