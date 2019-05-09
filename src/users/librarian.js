022a58b4bff585d8d6f8a09811d9876803a18a33
Tolu Adesina <tadesina90@gmail.com>
2019-05-09 10:48:39 +0100

    Librarian Constructor created and inherits from User Class

--- src/users/librarian.js
+++ src/users/librarian.js
@@ -1,3 +1,17 @@
 var db = require('../../helper/db');
 var User = require('./users');
+var Book = require('../books/books');
 var inherit = require('../../helper/extend');
+
+function Librarian(name) {
+  User.call(this, name);
+  this.role = 'Admin';
+}
+
+inherit(Librarian, User)
+
+Librarian.prototype.addBook = function() {
+  
+}
+
+module.exports = Librarian;
