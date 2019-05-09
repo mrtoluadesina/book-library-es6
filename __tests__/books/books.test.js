6c0e4c9ea185b002b0676f5b6b918ab2f883d8e6
Tolu Adesina <tadesina90@gmail.com>
2019-05-09 10:11:59 +0100

    Test Case - Checks the id auto increment, to make sure it increments per new book created

--- __tests__/books/books.test.js
+++ __tests__/books/books.test.js
@@ -2,6 +2,11 @@ var db = require('../../helper/db');
 var Book = require('../../src/books/books');
 
 describe('Book Constructor Tests', function() {
+  it('checks the id autoincrement if it works properly', function() {
+    var got = new Book('A Clash of Kings', 5);
+    expect(got.id).toBe(1);
+  });
+
   it('checks that a book is created', function() {
     var dbLength = db.books.length;
     console.log(dbLength);
