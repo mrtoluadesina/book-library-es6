b7527e2f29b33389a38236f1f22f2120a90737c0
Tolu Adesina <tadesina90@gmail.com>
2019-05-10 18:26:43 +0100

    Added comments to the User Constructor - so I can give clarity where needed

--- src/users/users.js
+++ src/users/users.js
@@ -9,25 +9,30 @@ function User(name, email, userType = 'junior student', userPriority = 1) {
   if (userType === 'senior student') this.userPriority = 2;
   if (userType === 'junior student') this.userPriority = userPriority;
   this.isActive = true;
-  this.id = db.users.length > 0 ? db.users[db.users.length - 1].id + 1 : 1;
+  this.id = db.users.length > 0 ? db.users[db.users.length - 1].id + 1 : 1; // generates the UserId
   db.users.push(this);
 }
 
+// User prototype created as an object literal
 User.prototype = {
   constructor: User,
+  // function that returns the details of a User 
   readUser: function() {
     return this;
   },
+  // function to update the User
   update: function(name, email, userType) {
     this.name = name;
     this.email = email;
     this.userType = userType;
     return this;
   },
+  // delete function - changes the active state to false
   delete: function() {
     this.isActive = false;
     return 'User Deleted';
   },
+  // function to lookup a user by the name
   search: function(name) {
     for (var index = 0; index < db.users.length; index++) {
       if (db.users[index].name === name) {
@@ -35,12 +40,15 @@ User.prototype = {
       }
     }
   },
+  //fucntion to search for a book - simple calls the actual function from the Book prototype
   bookLookUp: function(name) {
     return Book.prototype.search(name);
   },
+  // function a user calls to find a book by it's Id
   getBookById: function(id) {
     return Book.prototype.read(id);
   },
+  // Main function a User calls when borrowing a book
   requestBook: function(id, duration) {
     var userId = this.id;
     var userName = this.name;
