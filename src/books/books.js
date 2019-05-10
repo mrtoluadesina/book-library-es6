d92e9c54404046b44c883c6519afb21f66f428ee
Tolu Adesina <tadesina90@gmail.com>
2019-05-10 19:39:23 +0100

    Added some more comments to the system - books.js file

--- src/books/books.js
+++ src/books/books.js
@@ -1,11 +1,12 @@
 var db = require('../../helper/db');
 var id = 1;
+
 function Book(name, quantity) {
   this.name = name;
   this.quantity = quantity;
   this.isActive = true;
   this.id = id;
-  id++;
+  id++; // increments id so when a new Book is created id is  + 1
 }
 
 Book.prototype = {
@@ -15,7 +16,7 @@ Book.prototype = {
   },
   read: function(id) {
     for (var index = 0; index < db.books.length; index++) {
-      if(db.books[index].id === id) { return db.books[index]; }
+      if(db.books[index].id === id) return db.books[index];  // checks if the id of the current item is same as the id from parameter
     } return 'No Such Book';
   },
   edit: function(name, quantity) {
@@ -29,16 +30,19 @@ Book.prototype = {
   },
   search: function(name) {
     for (var index = 0; index < db.books.length; index++) {
-      if (db.books[index].name === name && db.books[index].isActive === true) return db.books[index];
+      if (db.books[index].name === name && db.books[index].isActive === true) return db.books[index]; // checks if the name of the current item is same as the name from the parameter
     } return 'Book not found!'
   },
+  // function for requesting a book
   bookRequest: function(bookId, duration, userId, userName, userPriority) {
     var bookName = Book.prototype.read(bookId).name;
     var time = String(new Date()).replace(/\sG.+/, '');
     var requestStatus = 'processing';
+    // Add the request Object to the bookRequestLog table
     db.bookRequestLog.push({
       bookId, bookName, time, duration, userId, userName, userPriority, requestStatus
     }); 
+
     db.bookRequestLog.sort(function(a, b) {
       return b.userPriority - a.userPriority;
     });
