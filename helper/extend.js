73f2aa752e7340af0ee2893e4e4424f47155a80d
Tolu Adesina <tadesina90@gmail.com>
2019-05-15 10:53:44 +0100

    Extend function updated with comments explaining what the file is for

--- helper/extend.js
+++ helper/extend.js
@@ -1,3 +1,4 @@
+// This is the file to implement inheritance - created as a function to be called when needed.
 function inherit(child, parent) {
   child.prototype = Object.create(parent.prototype);
   child.prototype.constructor = child;
