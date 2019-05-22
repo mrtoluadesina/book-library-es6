8e28ff6366b1f3d6c114d21508e1a7b4dcfb6dd4
Tolu Adesina <tadesina90@gmail.com>
2019-05-22 16:03:22 +0100

    Updated the book constructor to class - in ES6. exported it also in es6 format

--- src/books/books.js
+++ src/books/books.js
@@ -1,73 +1,69 @@
-var db = require('../../database/db');
+import * as db from '../../database/db';
 var thisBookId = 1;
 
-function Book(name, quantity) {
-  this.name = name;
-  this.quantity = quantity;
-  this.isActive = true;
-  this.id = thisBookId;
-  // increments id so when a new Book is created id is  + 1
-  thisBookId++;
-}
+class Book {
+  constructor(name, quantity) {
+    this.name = name;
+    this.quantity = quantity;
+    this.isActive = true;
+    this.id = thisBookId;
+    // increments id so when a new Book is created id is  + 1
+    thisBookId++;
+  }
 
-Book.prototype = {
-  constructor: Book,
-
-  create: function () {
+  create() {
     return new Book;
-  },
+  }
 
-  read: function (id) {
-    for (var index = 0; index < db.books.length; index++) {
+  read(id) {
+    for (let index = 0; index < db.books.length; index++) {
       // checks if the id of the current item is same as the id from parameter
       if (db.books[index].id === id) return db.books[index];  
     } return 'No Such Book';
-  },
+  }
 
-  edit: function (id, name, quantity) {
-    var book = Book.prototype.read(id);
+  edit(id, name, quantity) {
+    let book = Book.prototype.read(id);
     book.name = name;
     book.quantity = quantity;
     return 'Book Updated';
-  },
+  }
 
-  delete: function () {
+  delete() {
     this.isActive = false;
     return 'Book Deleted';
-  },
+  }
 
-  search: function (name) {
+  search(name) {
     for (var index = 0; index < db.books.length; index++) {
       // checks if the name of the current item is same as the name from the parameter
       if (db.books[index].name === name && db.books[index].isActive === true) return db.books[index]; 
     } return 'Book not found!'
-  },
+  }
 
-  // function for requesting a book
-  bookRequest: function (bookId, duration, userId, userName, userPriority) {
-    var bookName = Book.prototype.read(bookId).name;
-    var time = String(new Date()).replace(/\sG.+/, '');
-    var requestStatus = 'processing';
+  bookRequest(bookId, duration, userId, userName, userPriority) {
+    let bookName = Book.prototype.read(bookId).name;
+    let time = String(new Date()).replace(/\sG.+/, '');
+    let requestStatus = 'processing';
     // Add the request Object to the bookRequestLog table
     db.bookRequestLog.push({
       bookId, bookName, time, duration, userId, userName, userPriority, requestStatus
     }); 
     return 'Your order is processing!';
-  },
+  }
 
-  // function to process request in the request log table
-  processRequest: function () {
+  processRequest() {
     // Sort the request table to make sure it handles the higher priority first
     db.bookRequestLog.sort(function(a, b) {
       return b.userPriority - a.userPriority;
     });
 
-    for (var index = 0; index < db.bookRequestLog.length; index++) {
+    for (let index = 0; index < db.bookRequestLog.length; index++) {
       // we first check to make sure it is still of a processing status
       if (db.bookRequestLog[index].requestStatus === 'processing') {
-        var availableCopies = Number(Book.prototype.read(db.bookRequestLog[index].bookId).quantity);
+        let availableCopies = Number(Book.prototype.read(db.bookRequestLog[index].bookId).quantity);
         if (availableCopies > 0) {
-          var borrowedBook = Book.prototype.read(db.bookRequestLog[index].bookId).name;
+          let borrowedBook = Book.prototype.read(db.bookRequestLog[index].bookId).name;
           // decrement the number of copies available by 1
           availableCopies -= 1; 
           // call the edit method to update the details of the book with new quantity
@@ -82,15 +78,16 @@ Book.prototype = {
         } 
       } 
     } 
-    return 'Batch Processed'
-  },
+    return 'Batch Processed';
+  }
 
-  returnABook: function(bookId) {
-    for (var index = 0; index < db.books.length; index++) {
+  returnABook(bookId) {
+    for (let index = 0; index < db.books.length; index++) {
       if (db.books[index].id === bookId) {
         db.books[index].quantity += 1;
       }
     } return 'Book Returned';
   }
 }
-module.exports = Book;
+
+export default Book;
