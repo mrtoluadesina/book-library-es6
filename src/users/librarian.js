4f12b72e1108eb8cfa78432ced25abea50fb707d
Tolu Adesina <tadesina90@gmail.com>
2019-05-22 17:12:25 +0100

    Converted the constructor to Class and extended the User class

--- src/users/librarian.js
+++ src/users/librarian.js
@@ -1,31 +1,28 @@
-var db = require('../../database/db');
-var User = require('./users');
-var Book = require('../books/books');
-var inherit = require('../../helper/extend');
+import * as db from '../../database/db';
+import User from './users';
+import Book from '../books/books'
 
-function Librarian(name, email) {
-  // calls the constructor from the User Class to create the properties needed here
-  User.call(this, name, email); 
-  this.userType = 'admin';
+class Librarian extends User {
+  constructor(name, email) {
+    super(name, email);
+    this.userType = 'admin';
+  }
+
+  addBook(name, quantity) {
+    let book = Book.prototype.create();
+    book.name = name;
+    book.quantity = quantity;
+    db.books.push(book);
+    return 'Book Created';
+  }
+
+  updateBook(id, name, quantity) {
+    return Book.prototype.edit(id, name, quantity);
+  }
+
+  approveRequest() {
+    return Book.prototype.processRequest();
+  }
 }
 
-// inherits from User Constructor so we can access the methods from User
-inherit(Librarian, User) 
-
-Librarian.prototype.addBook = function (name, quantity) {
-  var book = Book.prototype.create();
-  book.name = name;
-  book.quantity = quantity;
-  db.books.push(book);
-  return 'Book Created';
-}
-
-Librarian.prototype.updateBook = function (id, name, quantity) {
-  return Book.prototype.edit(id, name, quantity)
-}
-
-Librarian.prototype.approveRequest = function () {
-  return Book.prototype.processRequest()
-}
-
-module.exports = Librarian;
+export default Librarian;
