d82abebb0fa65cb4d994213ab0ed9dc5b550a377
Tolu Adesina <tadesina90@gmail.com>
2019-05-22 05:38:53 +0100

    Changed the import statement to es6 version and also changed var to let

--- tests/books/books.test.js
+++ tests/books/books.test.js
@@ -1,18 +1,18 @@
-var Book = require('../../src/books/books');
+import Book from '../../src/books/books';
 
 describe('Book Constructor Tests', () => {
   it('checks the id autoincrement if it works properly', () => {
-    var got = new Book('A Clash of Kings', 5);
+    let got = new Book('A Clash of Kings', 5);
     expect(got.id).toBe(1);
   });
 
   it('checks that a book is created', () => {
-    var got = new Book('A Game of Thrones', 20);
+    let got = new Book('A Game of Thrones', 20);
     expect(got).toEqual(expect.objectContaining({name: expect.stringMatching(/^[A-Za-z\s]+$/)}))
   });
   
   it('checks that a book is deleted', () => {
-    var got = new Book('A Feast of Crows', 3);
+    let got = new Book('A Feast of Crows', 3);
     expect(got.delete()).toBe('Book Deleted');
   });
 });
